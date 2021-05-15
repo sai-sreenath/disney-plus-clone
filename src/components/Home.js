cae9b3c37d4b33d9deae3c8a7d8db8034f47e1b3
sai-sreenath <ppvsaisreenath565@gmail.com>
2021-05-15 10:43:28 -0400

    Adding Image slider in carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,20 +5,21 @@ const Home = (props) => {
     return (
         <Container>
             <ImgSlider/>
-        </Container>
+        </Container> 
     );
 };
 
 const Container = styled.main`
-    position:relative;
-    min-height: calc(100vh - 25px);
+    position: relative;
+    min-height: calc(100vh - 250px);
     overflow-x: hidden;
     display: block;
     top: 72px;
     padding: 0 calc(3.5vw + 5px);
 
     &:after{
-        background: url("/images/home-background.png") center center/cover no-repeat fixed;
+        background: url("/images/home-background.png") center center/cover 
+            no-repeat fixed;
         content: "";
         position: absolute;
         inset: 0px;
